4e95b51c5041ff08cfab4b9ab25a64b1f361d558
kramonex <kramonex1@yandex.ru>
2023-03-25 17:32:57 +0300

    setup starting value and slider

--- Change color in view/ViewController.swift
+++ Change color in view/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     @IBOutlet var rgbRectangle: UIView!
     
@@ -15,15 +15,30 @@ class ViewController: UIViewController {
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
-    @IBOutlet weak var redValue: UILabel!
-    @IBOutlet weak var greenValue: UILabel!
-    @IBOutlet weak var blueValue: UILabel!
+    @IBOutlet var redValue: UILabel!
+    @IBOutlet var greenValue: UILabel!
+    @IBOutlet var blueValue: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        rgbRectangle.layer.cornerRadius = 20
+        redSliderAction()
+        greenSliderAction()
+        blueSliderAction()
 
     }
 
+    @IBAction func redSliderAction(){
+        redValue.text = redSlider.value.formatted()
+//        rgbRectangle.backgroundColor = redSlider
+    }
+    @IBAction func greenSliderAction(){
+        greenValue.text = greenSlider.value.formatted()
+    }
+    @IBAction func blueSliderAction(){
+        blueValue.text = blueSlider.value.formatted()
+    }
 
+    
 }
 
